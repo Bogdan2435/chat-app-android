977d5dfb12eb23bddf1188c7994b7c7085cd4193
StefanIsac <stefisac63@gmail.com>
2023-06-22 21:11:38 +0300

    -

--- app/src/main/java/com/example/chata_app/Login.kt
+++ app/src/main/java/com/example/chata_app/Login.kt
@@ -1,12 +1,20 @@
 package com.example.chata_app
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FacebookAuthProvider
+import com.facebook.AccessToken
+
 
 class Login : AppCompatActivity() {
 
@@ -14,7 +22,8 @@ class Login : AppCompatActivity() {
     private lateinit var edtPassword: EditText
     private lateinit var btnLogin: Button
     private lateinit var btnSignUp: Button
-
+    private lateinit var callbackManager: CallbackManager
+    private lateinit var btnFacebookLogin: LoginButton
     private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +38,7 @@ class Login : AppCompatActivity() {
         edtPassword = findViewById(R.id.edt_password)
         btnLogin = findViewById(R.id.btn_login)
         btnSignUp = findViewById(R.id.btn_signup)
+        btnFacebookLogin = findViewById(R.id.login_button)
 
         btnSignUp.setOnClickListener {
             val intent = Intent(this, SignUp::class.java)
@@ -42,6 +52,22 @@ class Login : AppCompatActivity() {
             login(email, password);
         }
 
+        // Initialize Facebook Login button
+        callbackManager = CallbackManager.Factory.create()
+        btnFacebookLogin.setPermissions("email", "public_profile")
+        btnFacebookLogin.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                handleFacebookAccessToken(loginResult.accessToken)
+            }
+
+            override fun onCancel() {
+                // [Optional] Code to run if the user cancels the login process.
+            }
+
+            override fun onError(error: FacebookException) {
+                // [Optional] Code to run if an error occurs during the login process.
+            }
+        })
     }
 
     private fun login(email: String, password: String){
@@ -58,4 +84,27 @@ class Login : AppCompatActivity() {
             }
     }
 
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val intent = Intent(this@Login, MainActivity::class.java)
+                    finish()
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this@Login, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Pass the activity result back to the Facebook SDK
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
 }
